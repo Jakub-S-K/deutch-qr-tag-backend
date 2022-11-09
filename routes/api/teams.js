464c1abf81249e5fb29de80f8695940409b2f08a
Jakub-S-K <jakub.stanula.kaczka@gmail.com>
2022-11-09 21:59:50 +0100

    add team id to response GET /teams

--- routes/api/teams.js
+++ routes/api/teams.js
@@ -1,7 +1,7 @@
 const Teams = require('../../schemas/schemas.js').teams;
 
 module.exports.getTeams = async function (req, res) {
-    const teams = await Teams.find().select('-_id -__v').populate({path: 'members', select: '-__v'});
+    const teams = await Teams.find().select('-__v').populate({path: 'members', select: '-__v'});
     
     if (!teams) {
         return res.sendStatus(404);
