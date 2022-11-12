17233c94181a38cf2b89b29fa9c6840488e4d12f
Jakub-S-K <jakub.stanula.kaczka@gmail.com>
2022-11-12 12:31:33 +0100

    deselect __v key from query

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ require('mongoose');
 const Users = require('../../schemas/schemas.js').users;
 
 module.exports.get = function(req, res) {
-    Users.find().sort().then(users => {
+    Users.find().select('-__v').sort().then(users => {
         if (users) {
             res.json(users)
         } else {
